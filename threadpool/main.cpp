fdaaeedafa9b7d77e453c54e85d0024a9491258b
王子豪 <185892713@qq.com>
2025-06-08 18:54:27 +0800

    threadpool_v2

--- threadpool/main.cpp
+++ threadpool/main.cpp
@@ -4,75 +4,134 @@
 #include <queue>
 using namespace std;
 const int NUM=4;
+#define SIZE 1024
 pthread_mutex_t my_lock = PTHREAD_MUTEX_INITIALIZER;
 bool close=false;
-queue<void(*)(int*)> q;
+class Thread;
+queue<void(*)(Thread*)> q;
 class Thread
 {
 public:
-    Thread(void* (*HandleFunc)(void*),int n=NUM)
-            :ids(n)
+    using func_t = function<void*(void*)>;
+private:
+    static void* start_routine(void* args)
     {
-        for(int i=0;i<n;i++)
-        {
-            ids[i]=i;
-            pthread_t tid;
-            pthread_create(&tid, nullptr,HandleFunc, &ids[i]);
-            tids.emplace_back(tid);
-        }
+        Thread* ctx=(Thread*) args;
+        return ctx->callback();
     }
-    void ThreadJoin()
+public:
+    Thread()
     {
-        for(auto tid : tids){
-            pthread_join(tid, nullptr);
-        }
+        char buffer[SIZE];
+        snprintf(buffer,sizeof buffer,"thread is %d",threadnum);
+        threadnum++;
+        _name=buffer;
     }
-    vector<pthread_t> tids;
-    vector<int> ids;
+    void start(func_t func,void* args= nullptr){
+        _func=func;
+        _args=args;
+        pthread_create(&_tid, nullptr,start_routine,this);
+    }
+    void* callback()
+    {
+        return _func(_args);
+    }
+    void join()
+    {
+        pthread_join(_tid, nullptr);
+    }
+    string _name;
+    pthread_t _tid;
+    func_t _func;
+    void* _args;
+    static int threadnum;
 };
-void task(void (*callback)(int*)){
+int Thread::threadnum=0;
+class ThreadPool
+{
+public:
+    using func_t = function<void*(void*)>;
+    void start()
+    {
+        for(const auto& t : _threads)
+        {
+            t->start(handldFunc,t);
+        }
+    }
+    void join()
+    {
+        for(const auto& t : _threads)
+        {
+            t->join();
+        }
+    }
+    static ThreadPool* GetInstance()
+    {
+        if(_tp== nullptr)
+        {
+            _tp=new ThreadPool();
+        }
+        return _tp;
+    }
+private:
+    static void* handldFunc(void* args)
+    {
+        Thread* td=(Thread*) args;
+        while(1) {
+            pthread_mutex_lock(&my_lock);
+            if(close&&q.empty()){
+                pthread_mutex_unlock(&my_lock);
+                break;
+            }
+            if (q.empty()) {
+                pthread_mutex_unlock(&my_lock);
+                continue;
+            }
+            function<void(Thread*)> func = q.front();
+            q.pop();
+            pthread_mutex_unlock(&my_lock);
+            func(td);
+        }
+        return 0;
+    }
+    ThreadPool(int n=NUM)
+    {
+        for(int i=0;i<n;++i)
+        {
+            _threads.emplace_back(new Thread());
+        }
+    }
+    vector<Thread*> _threads;
+    func_t _func;
+    static ThreadPool* _tp;
+};
+ThreadPool* ThreadPool::_tp=nullptr;
+void task(void (*callback)(Thread*)){
 
     //派发任务
     q.push(callback);
 }
-void Print1(int* name){
-    printf("线程%d正在执行任务：画画\n",*name);
+void Print1(Thread* td){
+    printf("%s正在执行:",td->_name.c_str());
+    printf("画画\n");
 }
-void Print2(int* name){
-    printf("线程%d正在执行任务：睡觉\n",*name);
-}
-void* handldFunc(void* args){
-    int* name=(int*)args;
-    while(1) {
-        pthread_mutex_lock(&my_lock);
-        if(close&&q.empty()){
-            pthread_mutex_unlock(&my_lock);
-            break;
-        }
-        if (q.empty()) {
-            pthread_mutex_unlock(&my_lock);
-            continue;
-        }
-        function<void(int*)> func = q.front();
-        q.pop();
-        pthread_mutex_unlock(&my_lock);
-        func(name);
-    }
-    return 0;
+void Print2(Thread* td){
+    printf("%s正在执行:",td->_name.c_str());
+    printf("睡觉\n");
 }
 
 
 int main() {
-    Thread p1(handldFunc);
+    ThreadPool::GetInstance()->start();
     task(Print1);
     task(Print2);
     task(Print2);
     task(Print1);
     task(Print2);
-
-
     close=true;
-    p1.ThreadJoin();
+    ThreadPool::GetInstance()->join();
+
+
 
     return 0;
 }
